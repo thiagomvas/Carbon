f186e88635226b4e4c5cf5beef9c291854f7793c
Thiago Menezes <thiagomvas@gmail.com>
2024-08-11 12:09:03 -0300

    Improved ui and events

--- Carbon.Desktop/Views/MainWindow.axaml.cs
+++ Carbon.Desktop/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Carbon.Core.Plugins;
 using Carbon.Desktop.ViewModels;
 using CommunityToolkit.Mvvm.ComponentModel;
@@ -16,5 +18,43 @@ namespace Carbon.Desktop.Views
             InitializeComponent();
         }
 
+        private bool _mouseDownForWindowMoving = false;
+        private PointerPoint _originalPoint;
+
+        private void InputElement_OnPointerMoved(object? sender, PointerEventArgs e)
+        {
+            if (!_mouseDownForWindowMoving) return;
+
+
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+            }
+
+            PointerPoint currentPoint = e.GetCurrentPoint(this);
+            Position = new PixelPoint(Position.X + (int)(currentPoint.Position.X - _originalPoint.Position.X),
+                Position.Y + (int)(currentPoint.Position.Y - _originalPoint.Position.Y));
+        }
+
+        private void InputElement_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+        {
+            if (WindowState == WindowState.FullScreen) return;
+
+            _mouseDownForWindowMoving = true;
+            _originalPoint = e.GetCurrentPoint(this);
+        }
+
+        private void InputElement_OnPointerReleased(object? sender, PointerReleasedEventArgs e)
+        {
+            _mouseDownForWindowMoving = false;
+        }
+
+        private void Binding(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+        {
+        }
+
+        private void Binding_1(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+        {
+        }
     }
 }
