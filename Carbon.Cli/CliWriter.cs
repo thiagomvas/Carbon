971a70115314a81ba821bee2ee8a3f075dc90ab4
Thiago Menezes <thiagomvas@gmail.com>
2025-01-15 11:22:30 -0300

    refactor: Remove ICliWriter and use static class instead

--- Carbon.Cli/CliWriter.cs
+++ Carbon.Cli/CliWriter.cs
@@ -1,50 +1,28 @@
-using Carbon.Core;
-
 namespace Carbon.Cli;
 
-
-public class CliWriter : ICliWriter
+public class CliWriter
 {
-    public void WriteSuccess(string message, params object[]? args)
+    public static void WriteSuccess(string message)
     {
         Console.ForegroundColor = ConsoleColor.Green;
         Console.Write("[SUCCESS]");
         Console.ResetColor();
-        Console.WriteLine($" {message}", args);
+        Console.WriteLine(message);
     }
     
-    public void WriteError(string message, params object[]? args)
+    public static void WriteError(string message)
     {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.Write("[ERROR]");
         Console.ResetColor();
-        Console.WriteLine($" {message}", args);
+        Console.WriteLine(message);
     }
     
-    public static void WriteWarning(string message, params object[]? args)
+    public static void WriteWarning(string message)
     {
         Console.ForegroundColor = ConsoleColor.Yellow;
         Console.Write("[WARNING]");
         Console.ResetColor();
-        Console.WriteLine($" {message}", args);
+        Console.WriteLine(message);
     }
-    
-    public void WriteInfo(string message, params object[]? args)
-    {
-        Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.Write("[INFO]");
-        Console.ResetColor();
-        Console.WriteLine($" {message}", args);
-    }
-    
-    public void WriteDebug(string message, params object[]? args)
-    {
-        Console.ForegroundColor = ConsoleColor.DarkGray;
-        Console.Write("[DEBUG]");
-        Console.ResetColor();
-        Console.WriteLine($" {message}", args);
-    }
-    
-    
-    
 }
